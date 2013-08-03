07a5c963e980bfc0806679947bcafd62b4866a53
Nishant Shrivastava <nishant@weboniselab.com>
2013-08-03 22:05:57 +0530

    change in projects add

--- app/Controller/ProjectsController.php
+++ app/Controller/ProjectsController.php
@@ -7,12 +7,12 @@ App::uses('AppController', 'Controller');
  */
 class ProjectsController extends AppController {
 
-/**
- * Helpers
- *
- * @var array
- */
-	public $helpers = array('Html', 'Form');
+    /**
+     * Helpers
+     *
+     * @var array
+     */
+    public $helpers = array('Html', 'Form');
 
 
     public function beforeFilter() {
@@ -20,92 +20,109 @@ class ProjectsController extends AppController {
         $this->Auth->allow('*');
     }
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->Project->recursive = 0;
-		$this->set('projects', $this->paginate());
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->Project->recursive = 0;
+        $this->set('projects', $this->paginate());
+    }
 
-/**
- * view method
- *
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		$this->set('project', $this->Project->read(null, $id));
-	}
+    /**
+     * view method
+     *
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        $this->set('project', $this->Project->read(null, $id));
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Project->create();
-			if ($this->Project->save($this->request->data)) {
-				$this->Session->setFlash(__('The project has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
-			}
-		}
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if (!empty($this->request->data) && $this->request->is('post')) {
+
+            pr($this->request->data);
+            exit;
+
+            $this->Project->create();
+            if ($this->Project->save($this->request->data)) {
+                $this->Session->setFlash(__('The project has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+            }
+        }
         $technologies = $this->Project->getTechnologyData();
-        pr($technologies);
         $this->set(compact('technologies'));
-	}
+    }
 
-/**
- * edit method
- *
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Project->save($this->request->data)) {
-				$this->Session->setFlash(__('The project has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Project->read(null, $id);
-		}
-	}
+    /**
+     * edit method
+     *
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Project->save($this->request->data)) {
+                $this->Session->setFlash(__('The project has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->Project->read(null, $id);
+        }
+    }
 
-/**
- * delete method
- *
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		if ($this->Project->delete()) {
-			$this->Session->setFlash(__('Project deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Project was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+    /**
+     * delete method
+     *
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        if ($this->Project->delete()) {
+            $this->Session->setFlash(__('Project deleted'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Project was not deleted'));
+        $this->redirect(array('action' => 'index'));
+    }
+
+    public function getAllTechnologies() {
+        $this->autoRender = false;
+        if ($this->request->is('get') && empty($this->request->data)) {
+            $technologies = array();
+            $technologies = $this->Project->getTechnologyData();
+            if (!empty($technologies)) {
+                return json_encode($technologies);
+
+            } else {
+                return json_encode(array());
+            }
+        }
+    }
 }
--- app/View/Helper/RelocateHelper.php
+++ app/View/Helper/RelocateHelper.php
@@ -1,6 +1,8 @@
 <?php
-App::uses('AppHelper', 'Helper');
+App::uses('AppHelper', 'View/Helper');
 class RelocateHelper extends AppHelper {
+    public $helpers = array();
+
     public function allocationPercentage() {
         $percentage = array();
         for ($i = 0; $i = 100; $i + 5) {
